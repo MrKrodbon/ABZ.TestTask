18aa641634b1e8b20a733397c03abb243cc64e6e
MrKrodbon <tir25@ukr.net>
2025-09-18 23:20:12 +0300

    Fix publicPath for GitHub Pages

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
-    publicPath: process.env.NODE_ENV === "production" ? "/ABZ.TestTask/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
   },
   devtool: "inline-source-map",
   devServer: {
